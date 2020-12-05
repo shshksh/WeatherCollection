a6e08358361046817354248f619ff2efd2c32c2e
seonghwan <ksh970516@pukyong.ac.kr>
2020-12-05 23:09:41 +0900

    Initialize project

---
+++ app/src/main/java/com/mob/weathercollection/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mob.weathercollection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mob.weathercollection.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
