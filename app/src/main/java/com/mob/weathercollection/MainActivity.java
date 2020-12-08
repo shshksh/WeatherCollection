2aefc59b18ebe6d478706f17d3855d65b8fb7312
seonghwan <ksh970516@pukyong.ac.kr>
2020-12-08 14:32:18 +0900

    Add temp items to weather layout

--- app/src/main/java/com/mob/weathercollection/MainActivity.java
+++ app/src/main/java/com/mob/weathercollection/MainActivity.java
@@ -1,26 +1,53 @@
 package com.mob.weathercollection;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.mob.weathercollection.databinding.ActivityMainBinding;
+import com.mob.weathercollection.databinding.ItemTempperhoursBinding;
+import com.mob.weathercollection.model.weather.Data;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    final int CHOOSE_LOCATION_REQUEST = 5001;
+    MainViewModel mainViewModel;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-        MainViewModel mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         binding.setLifecycleOwner(this);
         binding.setVm(mainViewModel);
 
-//        LinearLayout llItemMain = binding.layoutKma.llItammainTemperatureperhour;
-//        View itemTemp = getLayoutInflater().inflate(R.layout.item_tempperhours, llItemMain, false);
-//        llItemMain.addView(itemTemp);
+        binding.layoutKma.btnItemmainChooselocation.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ChooseLocationActivity.class);
+            startActivityForResult(intent, CHOOSE_LOCATION_REQUEST);
+        });
+
+        mainViewModel.getKmaWeather().observe(this, weather -> addTempItems(weather.channel.item.description.body.data));
+    }
+
+    private void addTempItems(List<Data> temps) {
+        LinearLayout itemLayout = binding.layoutKma.llItammainTemperatureperhour;
+        itemLayout.removeAllViews();
+        for (Data temp : temps) {
+            @NonNull ItemTempperhoursBinding itemBinding = ItemTempperhoursBinding.inflate(getLayoutInflater(), itemLayout, false);
+            Log.d("Add kma item: data", temp.temp + " " + temp.hour);
+            itemBinding.tvItemtempperhoursTemp.setText(temp.temp);
+            itemBinding.tvItemtempperhoursHour.setText("" + temp.hour);
+            Log.d("Add kma item: layout", itemBinding.tvItemtempperhoursTemp.getText().toString() + " " + itemBinding.tvItemtempperhoursHour.getText().toString());
+            itemLayout.addView(itemBinding.getRoot());
+        }
     }
 }
